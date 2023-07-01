5ff97a047aabacb8ea6ff1d551bf8abd6997c5dc
Gavin Haseltine <gavinhaseltine@gmail.com>
2023-06-30 22:27:55 -0500

    almost done

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,9 +1,20 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket =[]
+console.log(basket);
 
+function addItem( item ){
+console.log( 'In addItem ', item );
+//push an item into the basket array
+basket.push( item );
+return true;
+} //End addItem
 
+function listItems(){
+    console.log( 'In listItems', basket);
 
+} //end listItems
 
 
 
