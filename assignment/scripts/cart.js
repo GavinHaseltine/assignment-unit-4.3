a79a7ecc0749d1322051f7f7d2a0cd329f7f07e3
Gavin Haseltine <gavinhaseltine@gmail.com>
2023-07-06 19:10:49 -0500

    corrected

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -5,6 +5,8 @@ let basket =[]
 console.log(basket);
 let i = 0;
 
+//Function to add an item to the basket array 
+//start addItem
 function addItem( item ){
 console.log( 'In addItem ', item );
 //push an item into the basket array
@@ -12,6 +14,10 @@ basket.push( item );
 return true;
 } //End addItem
 
+addItem("Fruit"); //end Fruit
+addItem("Milk"); //end Milk
+
+//Function to list the items in the basket array
 function listItems(){
     console.log( 'In listItems', basket);
 while(i < basket.length){
@@ -21,12 +27,16 @@ while(i < basket.length){
 }
 } //end listItems
 
+listItems(); 
+
+//function to empty the basket array
 function empty(){
-    let basket = [];
-    console.log(basket);
+     basket = [];
+    console.log("In empty", basket);
 }
 
-
+empty();
+console.log(basket);
 
 
 
